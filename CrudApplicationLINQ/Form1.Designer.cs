91b0d3334a0740a845b6da4680ccda1efbd49197
gadiyevich <48290063+gadiyevich@users.noreply.github.com>
2019-08-14 02:05:37 +0400

    Add project files.

---
+++ CrudApplicationLINQ/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace CrudApplicationLINQ
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Price = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Show = new System.Windows.Forms.Button();
+            this.DataView = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Maded_by = new System.Windows.Forms.TextBox();
+            this.Category = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Insert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(601, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Maded_by";
+            // 
+            // Price
+            // 
+            this.Price.AutoSize = true;
+            this.Price.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price.Location = new System.Drawing.Point(963, 305);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(53, 20);
+            this.Price.TabIndex = 46;
+            this.Price.Text = "Price";
+            this.Price.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Date
+            // 
+            this.Date.AutoSize = true;
+            this.Date.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Date.Location = new System.Drawing.Point(789, 305);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(49, 20);
+            this.Date.TabIndex = 45;
+            this.Date.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(440, 305);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 20);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Category";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(308, 305);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Name";
+            // 
+            // Show
+            // 
+            this.Show.AccessibleRole = System.Windows.Forms.AccessibleRole.Animation;
+            this.Show.BackColor = System.Drawing.Color.Red;
+            this.Show.Font = new System.Drawing.Font("Bahnschrift", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Show.Location = new System.Drawing.Point(526, 19);
+            this.Show.Name = "Show";
+            this.Show.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Show.Size = new System.Drawing.Size(205, 87);
+            this.Show.TabIndex = 42;
+            this.Show.Text = "Show";
+            this.Show.UseVisualStyleBackColor = false;
+            this.Show.Click += new System.EventHandler(this.Show_Click);
+            // 
+            // DataView
+            // 
+            this.DataView.BackgroundColor = System.Drawing.Color.Snow;
+            this.DataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataView.GridColor = System.Drawing.Color.MediumBlue;
+            this.DataView.Location = new System.Drawing.Point(222, 403);
+            this.DataView.Name = "DataView";
+            this.DataView.RowHeadersWidth = 51;
+            this.DataView.RowTemplate.Height = 24;
+            this.DataView.Size = new System.Drawing.Size(873, 257);
+            this.DataView.TabIndex = 41;
+            this.DataView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataView_CellClick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.numericUpDown1.Location = new System.Drawing.Point(936, 333);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 30);
+            this.numericUpDown1.TabIndex = 40;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(741, 332);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(159, 32);
+            this.dateTimePicker1.TabIndex = 39;
+            // 
+            // Maded_by
+            // 
+            this.Maded_by.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.Maded_by.Location = new System.Drawing.Point(586, 333);
+            this.Maded_by.Multiline = true;
+            this.Maded_by.Name = "Maded_by";
+            this.Maded_by.Size = new System.Drawing.Size(123, 31);
+            this.Maded_by.TabIndex = 38;
+            // 
+            // Category
+            // 
+            this.Category.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.Category.Location = new System.Drawing.Point(426, 332);
+            this.Category.Multiline = true;
+            this.Category.Name = "Category";
+            this.Category.Size = new System.Drawing.Size(116, 31);
+            this.Category.TabIndex = 37;
+            // 
+            // Name
+            // 
+            this.Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name.Location = new System.Drawing.Point(286, 333);
+            this.Name.Multiline = true;
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(106, 31);
+            this.Name.TabIndex = 36;
+            // 
+            // Update
+            // 
+            this.Update.BackColor = System.Drawing.Color.White;
+            this.Update.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Update.Location = new System.Drawing.Point(724, 121);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(101, 43);
+            this.Update.TabIndex = 35;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = false;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.BackColor = System.Drawing.Color.White;
+            this.Delete.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete.Location = new System.Drawing.Point(585, 121);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(100, 43);
+            this.Delete.TabIndex = 34;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Insert
+            // 
+            this.Insert.BackColor = System.Drawing.Color.White;
+            this.Insert.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Insert.Location = new System.Drawing.Point(451, 121);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(101, 43);
+            this.Insert.TabIndex = 33;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = false;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1322, 739);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Show);
+            this.Controls.Add(this.DataView);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Maded_by);
+            this.Controls.Add(this.Category);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Insert);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Price;
+        private System.Windows.Forms.Label Date;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Show;
+        private System.Windows.Forms.DataGridView DataView;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox Maded_by;
+        private System.Windows.Forms.TextBox Category;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Insert;
+    }
+}
+
---
+++ CrudApplicationLINQ/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudApplicationLINQ
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void Show_Click(object sender, EventArgs e)
+        {
+            DataLinqToSqlDataContext linq = new DataLinqToSqlDataContext();
+
+            DataView.DataSource = linq.PRODUCTs;
+
+            Insert.Visible = true;
+            Delete.Visible = true;
+            Update.Visible = true;
+            DataView.Visible = true;
+            Maded_by.Visible = true;
+            Name.Visible = true;
+            Category.Visible = true;
+            dateTimePicker1.Visible = true;
+            numericUpDown1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            Date.Visible = true;
+            Price.Visible = true;
+            label1.Visible = true;
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataLinqToSqlDataContext dbinstall = new DataLinqToSqlDataContext();
+
+                PRODUCT prod = new PRODUCT();
+
+                prod.NAME = Name.Text;
+                prod.CATEGORY = Category.Text;
+                prod.MADED_BY = Maded_by.Text;
+                prod.PRODUCT_DATE = dateTimePicker1.Value;
+                prod.PRICE = numericUpDown1.Value;
+
+                dbinstall.PRODUCTs.InsertOnSubmit(prod);
+
+                dbinstall.SubmitChanges();
+
+                DataView.DataSource = dbinstall.PRODUCTs;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                DataLinqToSqlDataContext dbdelete = new DataLinqToSqlDataContext();
+
+                int prodID = Convert.ToInt32(DataView.CurrentRow.Cells["ID"].Value);
+
+                PRODUCT prod = dbdelete.PRODUCTs.SingleOrDefault(o => o.ID == prodID);
+                dbdelete.PRODUCTs.DeleteOnSubmit(prod);
+                dbdelete.SubmitChanges();
+                DataView.DataSource = dbdelete.PRODUCTs;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong! Retry please ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataLinqToSqlDataContext dbupcon = new DataLinqToSqlDataContext();
+                int prodId = Convert.ToInt32(Name.Tag);
+
+                PRODUCT prod = dbupcon.PRODUCTs.SingleOrDefault(product => product.ID == prodId);
+
+                prod.NAME = Name.Text;
+                prod.CATEGORY = Category.Text;
+                prod.MADED_BY = Maded_by.Text;
+                prod.PRODUCT_DATE = Convert.ToDateTime(dateTimePicker1.Text);
+                prod.PRICE = numericUpDown1.Value;
+
+                dbupcon.SubmitChanges();
+
+                DataView.DataSource = dbupcon.PRODUCTs;
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong :( , please try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+            }
+        }
+
+        private void DataView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+
+                DataGridViewRow Row = DataView.CurrentRow;
+                Name.Text = Row.Cells["Name"].Value.ToString();
+
+                Name.Tag = Row.Cells["ID"].Value;
+
+                Category.Text = Row.Cells["CATEGORY"].Value.ToString();
+                Maded_by.Text = Row.Cells["MADED_BY"].Value.ToString();
+                dateTimePicker1.Text = Row.Cells["PRODUCT_DATE"].Value.ToString();
+                numericUpDown1.Value = (decimal)Row.Cells["PRICE"].Value;
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to my Application. In this Application you can Create, Insert, Update and Delete any data you added. Good luck :)","INFORMATION",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+            Insert.Visible = false;
+            Delete.Visible = false;
+            Update.Visible = false;
+            DataView.Visible = false;
+            Name.Visible = false;
+            Category.Visible = false;
+            Maded_by.Visible = false;
+            dateTimePicker1.Visible = false;
+            numericUpDown1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            Date.Visible = false;
+            Price.Visible = false;
+            label1.Visible = false;
+        }
+    }
+}
